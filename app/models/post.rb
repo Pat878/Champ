faceafbeb625ac40387c0546af2904f4aa35589b
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-16 21:22:51 -0500

    Display factorial in show action

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  before_save   :add_factorial
+
+  def add_factorial
+
+    def random_factorial
+      number = rand(1..10)
+      def factorial(number_argument)
+        if number_argument == 0
+          1
+        else
+          number_argument * factorial(number_argument - 1);
+        end
+      end
+      factorial(number)
+    end
+    self.number = random_factorial
+  end
+
 end
---
+++ db/migrate/20180117015301_add_number_to_posts.rb
@@ -0,0 +1,5 @@
+class AddNumberToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_column :posts, :number, :integer
+  end
+end
